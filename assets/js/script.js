aafae620ef289dca97ed61da44db76e961a1be68
ParboldCoder <darren.a.buck@gmail.com>
2024-01-31 20:35:25 +0000

    successful API and Map Data search

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,9 @@ document.addEventListener('DOMContentLoaded', function () {
             if (response.ok) {
                 const data = await response.json();
                 console.log(data);
+                const lon = data.items[0].position.lng;
+                const lat = data.items[0].position.lat;
+                moveMapToLocation(map, lat, lon);
             } else {
                 console.error('Failed to fetch data. Status:', response.status);
             }
@@ -18,6 +21,29 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
+    function moveMapToLocation(map, lat, lon){
+        map.setCenter({lat: lat, lng: lon});
+        map.setZoom(16);
+    }
+
+    const platform = new H.service.Platform({
+        apikey: `${apiKey}`
+    });
+    
+    const defaultLayers = platform.createDefaultLayers();
+  
+    var map = new H.Map(document.getElementById('map'),
+        defaultLayers.vector.normal.map,{
+        center: {lat:50, lng:5},
+        zoom: 4,
+        pixelRatio: window.devicePixelRatio || 1
+    });
+    window.addEventListener('resize', () => map.getViewPort().resize());
+    
+    var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+    
+    var ui = H.ui.UI.createDefault(map, defaultLayers);
+      
     document.getElementById('searchButton').addEventListener('click', function () {
         const location = document.getElementById('locationInput').value;
         fetchData(location);
