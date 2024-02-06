8c0f0c3f2e88d88f070ce02c72a04496cb28516e
jgandhi2002 <jai.2002.gandhi@gmail.com>
2024-02-06 20:21:41 +0000

    grab info from fetch

--- assets/js/geoapify.js
+++ assets/js/geoapify.js
@@ -9,7 +9,23 @@ function getPlaces(){
       fetch(`https://api.geoapify.com/v2/places?categories=${categories}&conditions=${conditions}&filter=circle:${lon},${lat},${radius}&bias=proximity:${lon},${lat}&lang=en&limit=20&apiKey=fe9a326d269345a4b9e1136bfdae6a47`)
         .then(response => response.json())
         .then(function loadCards(result){
+            console.log(`https://api.geoapify.com/v2/places?categories=${categories}&conditions=${conditions}&filter=circle:${lon},${lat},${radius}&bias=proximity:${lon},${lat}&lang=en&limit=20&apiKey=fe9a326d269345a4b9e1136bfdae6a47`)
             console.log(result)
+            result.features.forEach(searchResult => {
+              console.log(`Name: ${searchResult.properties.name}`)
+              console.log(`Address: ${searchResult.properties.address_line2}`)
+              if (!searchResult.properties.datasource.raw.website) {
+                console.log("No website")
+              } else {
+                console.log(`Website: ${searchResult.properties.datasource.raw.website}`)
+              }
+              console.log(`Opening Hours: ${searchResult.properties.datasource.raw.opening_hours}`)
+              console.log(`Wheelchair Accessible: ${searchResult.properties.datasource.raw.wheelchair}`)
+              console.log(`Distance: ${searchResult.properties.distance}m`)
+            });
         })
         .catch(error => console.log('error', error));
 }
+
+
+// name location distance link
